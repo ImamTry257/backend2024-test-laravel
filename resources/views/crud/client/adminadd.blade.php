55ab9a10d36b64e4698cdb4ef18300786acc66d6
imamtry <imamtrywibowo@gmail.com>
2024-02-13 17:22:48 +0700

    add form on add client

--- resources/views/crud/client/adminadd.blade.php
+++ resources/views/crud/client/adminadd.blade.php
@@ -10,33 +10,40 @@
                     </div>
     
                     <div class="card-body">
-                        <div class="action-add pb-3">
-                            <a href="{{ route('admin') }}" class="btn btn-primary">Back</a>
-                        </div>
-                        <div class="form-add">
-                            <form>
-                                <div class="mb-3">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input type="text" class="form-control" id="name" name="u_name">
-                                </div>
-                                
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Is Projec</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1">
-                                </div>
+                        <div class="col-8">
+                            <div class="action-add pb-3">
+                                <a href="{{ route('admin') }}" class="btn btn-primary">Back</a>
+                            </div>
+                            <div class="form-add">
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Name</label>
+                                        <input type="text" class="form-control" id="name" name="u_name">
+                                    </div>
+    
+                                    <div class="mb-3 form-check">
+                                        <input type="checkbox" class="form-check-input" id="is_project">
+                                        <label class="form-check-label" for="is_project">Is Project</label>
+                                    </div>
+    
+                                    <div class="mb-3">
+                                        <label for="address" class="form-label">Address</label>
+                                        <textarea name="address" id="address" class="form-control" cols="30" rows="10"></textarea>
+                                    </div>
+    
+                                    <div class="mb-3">
+                                        <label for="phone_number" class="form-label">Phone Number</label>
+                                        <input type="number" class="form-control" id="phone_number">
+                                    </div>
 
-                                <div class="mb-3">
-                                    <label for="address" class="form-label">Address</label>
-                                    <textarea name="address" id="address" class="form-control" cols="30" rows="10"></textarea>
-                                </div>
-
-                                <div class="mb-3">
-                                    <label for="phone_number" class="form-label">Phone Number</label>
-                                    <input type="number" class="form-control" id="phone_number">
-                                </div>
-
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                              </form>
+                                    <div class="mb-3">
+                                        <label for="city" class="form-label">City</label>
+                                        <input type="text" class="form-control" id="city">
+                                    </div>
+    
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                  </form>
+                            </div>
                         </div>
                     </div>
                 </div>
